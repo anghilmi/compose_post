00fbf1ba040b933670297283101a8306ce039cdd
anghilmi <alhilmi1@gmail.com>
2023-06-11 20:23:24 +0700

    first commit

---
+++ app/src/main/java/com/example/apicompose/DataModel.kt
@@ -0,0 +1,13 @@
+package com.example.apicompose
+//
+//class DataModel {
+//    // on below line we are creating variables for name and job
+//    var name: String,
+//    var job: String
+//}
+
+data class DataModel(
+    // on below line we are creating variables for name and job
+    var nama_produk: String,
+    var deskripsi_produk: String
+)
--- app/src/main/java/com/example/apicompose/MainActivity.kt
+++ app/src/main/java/com/example/apicompose/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.apicompose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.*
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import com.example.apicompose.ui.theme.ApiComposeTheme
+import java.util.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +39,40 @@ class MainActivity : ComponentActivity() {
             ApiComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    // on below line we are specifying modifier and color for our app
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    // on the below line we are specifying the theme as the scaffold.
+                    Scaffold(
+                        // in scaffold we are specifying top bar.
+                        topBar = {
+
+                            // inside top bar we are specifying background color.
+                            val greenColor = Color(0xFF0F9D58)
+                            TopAppBar(backgroundColor = greenColor,
+
+                                // along with that we are specifying title for our top bar.
+                                title = {
+
+                                    // in the top bar we are specifying tile as a text
+                                    Text(
+                                        // on below line we are specifying text to display in top app bar.
+                                        text = "Retrofit POST Request in Android",
+
+                                        // on below line we are specifying modifier to fill max width.
+                                        modifier = Modifier.fillMaxWidth(),
+
+                                        // on below line we are specifying text alignment.
+                                        textAlign = TextAlign.Center,
+
+                                        // on below line we are specifying color for our text.
+                                        color = Color.White
+                                    )
+                                })
+                        }) {
+                        // on the below line we are calling the pop window dialog method to display ui.
+                        postData()
+                    }
                 }
             }
         }
@@ -30,14 +80,151 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun postData() {
+    val ctx = LocalContext.current
+
+    val namaProduk = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val deskripsiProduk = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val response = remember {
+        mutableStateOf("")
+    }
+    // on below line we are creating a column.
+    Column(
+        // on below line we are adding a modifier to it
+        // and setting max size, max height and max width
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        // on below line we are adding vertical
+        // arrangement and horizontal alignment.
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // on below line we are creating a text
+//        val greenColor = Color(0xFF0F9D58)
+
+        //on below line we are adding spacer
+        Spacer(modifier = Modifier.height(5.dp))
+        // on below line we are creating a text field for our email.
+        TextField(
+            // on below line we are specifying value for our email text field.
+            value = namaProduk.value,
+            // on below line we are adding on value change for text field.
+            onValueChange = { namaProduk.value = it },
+            // on below line we are adding place holder as text as "Enter your email"
+            placeholder = { Text(text = "Enter nama prod") },
+            // on below line we are adding modifier to it
+            // and adding padding to it and filling max width
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            // on below line we are adding text style
+            // specifying color and font size to it.
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+            // on below line we are adding single line to it.
+            singleLine = true,
+        )
+        // on below line we are adding spacer
+        Spacer(modifier = Modifier.height(5.dp))
+        // on below line we are creating a text field for our email.
+        TextField(
+            // on below line we are specifying value for our email text field.
+            value = deskripsiProduk.value,
+            // on below line we are adding on value change for text field.
+            onValueChange = { deskripsiProduk.value = it },
+            // on below line we are adding place holder as text as "Enter your email"
+            placeholder = { Text(text = "Enter desk prod") },
+            // on below line we are adding modifier to it
+            // and adding padding to it and filling max width
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            // on below line we are adding text style
+            // specifying color and font size to it.
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+            // on below line we ar adding single line to it.
+            singleLine = true,
+        )
+        // on below line we are adding spacer
+        Spacer(modifier = Modifier.height(10.dp))
+        // on below line we are creating a button
+        Button(
+            onClick = {
+                // on below line we are calling make payment method to update data.
+                postDataUsingRetrofit(
+                    ctx, namaProduk, deskripsiProduk, response
+                )
+            },
+            // on below line we are adding modifier to our button.
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            // on below line we are adding text for our button
+            Text(text = "Post Data", modifier = Modifier.padding(8.dp))
+        }
+        // on below line we are adding a spacer.
+        Spacer(modifier = Modifier.height(20.dp))
+        // on below line we are creating a text for displaying a response.
+        Text(
+            text = response.value,
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold, modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ApiComposeTheme {
-        Greeting("Android")
-    }
+private fun postDataUsingRetrofit(
+    ctx: Context,
+    namaProduk: MutableState<TextFieldValue>,
+    deskripsiProduk: MutableState<TextFieldValue>,
+    result: MutableState<String>
+) {
+    var url = "http://192.168.43.128:8000/api/"
+    // on below line we are creating a retrofit
+    // builder and passing our base url
+    val retrofit = Retrofit.Builder()
+        .baseUrl(url)
+        // as we are sending data in json format so
+        // we have to add Gson converter factory
+        .addConverterFactory(GsonConverterFactory.create())
+        // at last we are building our retrofit builder.
+        .build()
+    // below the line is to create an instance for our retrofit api class.
+    val retrofitAPI = retrofit.create(RetrofitAPI::class.java)
+    // passing data from our text fields to our model class.
+    val dataModel = DataModel(namaProduk.value.text, deskripsiProduk.value.text)
+    // calling a method to create an update and passing our model class.
+    val call: Call<DataModel?>? = retrofitAPI.postData(dataModel)
+    // on below line we are executing our method.
+    call!!.enqueue(object : Callback<DataModel?> {
+        override fun onResponse(call: Call<DataModel?>?, response: Response<DataModel?>) {
+            // this method is called when we get response from our api.
+            Toast.makeText(ctx, "Data posted to API", Toast.LENGTH_SHORT).show()
+            // we are getting a response from our body and
+            // passing it to our model class.
+            val model: DataModel? = response.body()
+            // on below line we are getting our data from model class
+            // and adding it to our string.
+            val resp =
+                "Response Code : " + response.code() + "\n" + "Nama produk : " + model!!.nama_produk + "\n" + "Desk produk : " + model!!.deskripsi_produk
+            // below line we are setting our string to our response.
+            result.value = resp
+        }
+
+        override fun onFailure(call: Call<DataModel?>?, t: Throwable) {
+            // we get error response from API.
+            result.value = "Error found is : " + t.message
+        }
+    })
+
 }
