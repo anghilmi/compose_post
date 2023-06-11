977f3122a861acddccde3ded3df6c0ab5c5c2dce
Muhammad Anis Al Hilmi <alhilmi1@gmail.com>
2023-06-11 21:02:22 +0700

    Update DataModel.kt

--- app/src/main/java/com/example/apicompose/DataModel.kt
+++ app/src/main/java/com/example/apicompose/DataModel.kt
@@ -1,13 +1,10 @@
 package com.example.apicompose
-//
-//class DataModel {
-//    // on below line we are creating variables for name and job
-//    var name: String,
-//    var job: String
-//}
+
 
 data class DataModel(
     // on below line we are creating variables for name and job
     var nama_produk: String,
-    var deskripsi_produk: String
+    var deskripsi_produk: String,
+    var harga_produk: String,
+    var url_produk: String
 )
